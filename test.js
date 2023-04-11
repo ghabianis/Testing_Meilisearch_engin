2c4e02c95989aa061074adb16e45ef3f28e78c9f
Anis Ghabi <anisghabi8@gmail.com>
2023-04-11 09:56:10 +0000

    env_file_config

--- test.js
+++
@@ -1,7 +0,0 @@
-const { MeiliSearch } = require('meilisearch')
-const movies = require('./movies.json')
-
-
-const client = new MeiliSearch({ host: 'http://localhost:7700' })
-client.index('movies').addDocuments(movies)
-  .then((res) => console.log(res))
